c4f234f77ee7ed8ff028c87fee02f2ab6b887748
Adriana <adriandy2005@gmail.com>
2025-07-15 04:53:09 -0500

    Una noche mas

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,19 +1,8 @@
-// src/users/dto/create-user.dto.ts
-import { IsString, IsEmail } from 'class-validator';
-
 export class CreateUserDto {
-  @IsString()
   nombre: string;
-
-  @IsEmail()
   correo: string;
-
-  @IsString()
   celular: string;
-
-  @IsString()
   username: string;
-
-  @IsString()
   password: string;
+  role?: 'admin' | 'user';
 }
