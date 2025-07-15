a14e61b149f6e98ec910205dd037eff4c95ec7ad
Adriana <adriandy2005@gmail.com>
2025-07-15 04:12:06 -0500

    CRUD

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ✅ Habilita CORS para Netlify (producción) y localhost (desarrollo)
+  app.enableCors({
+    origin: [
+      'https://casaempanadagigante.netlify.app', // producción
+      'http://localhost:5173',                   // desarrollo
+    ],
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
-  const PORT = process.env.PORT || 3000; // ✅ para Render
+
+  const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
 }
 bootstrap();
