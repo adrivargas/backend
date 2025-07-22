744a5469fca0bfca414965ac1fdda6e19e62b1ed
Adriana <adriandy2005@gmail.com>
2025-07-22 14:01:04 -0500

    CORS

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'https://casaempanadagigante.netlify.app', 
+      'https://casaempanadagigante.vercel.app', 
       'http://localhost:5173',                   
     ],
     credentials: true,
